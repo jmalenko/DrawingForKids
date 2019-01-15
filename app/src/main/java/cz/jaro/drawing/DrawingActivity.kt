4309a26981675c7f59d2937812a77ce81e528ae8
Jaromir Malenko <jaro@jaro.cz>
2019-01-15 20:35:43 +0100

    Fix typo

--- app/src/main/java/cz/jaro/drawing/DrawingActivity.kt
+++ app/src/main/java/cz/jaro/drawing/DrawingActivity.kt
@@ -72,7 +72,7 @@ class DrawingActivity : Activity() {
     private fun keyCodeToString(action: Int): String {
         return when (action) {
             KeyEvent.KEYCODE_BACK -> "Back"
-            KeyEvent.KEYCODE_VOLUME_UP -> "Volumne up"
+            KeyEvent.KEYCODE_VOLUME_UP -> "Volune up"
             KeyEvent.KEYCODE_VOLUME_DOWN -> "Volume down"
             else -> "?"
         }
