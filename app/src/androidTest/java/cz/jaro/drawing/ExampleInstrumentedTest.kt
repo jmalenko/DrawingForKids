80682f8a1d4e5c0b90ab4b32aac2b76e4ff4ac82
Jaromir Malenko <jaro@jaro.cz>
2019-01-21 17:02:26 +0100

    Fix test

--- app/src/androidTest/java/cz/jaro/drawing/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/cz/jaro/drawing/ExampleInstrumentedTest.kt
@@ -8,8 +8,6 @@ import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
@@ -17,6 +15,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("cz.jaro.drawingforkids", appContext.packageName)
+        assertEquals("cz.jaro.drawing", appContext.packageName)
     }
 }
